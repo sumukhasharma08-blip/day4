4903856d474a134d77bfa5fef67c5f326df59d13
sumukhasharma08-blip <sumukhasharma08@gmail.com>
2026-01-30 15:25:37 +0530

    Add files via upload

---
+++ code14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class code14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int N = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
