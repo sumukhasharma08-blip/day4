80a8f7017915f2971e89cb19197d27aa11b2ea68
sumukhasharma08-blip <sumukhasharma08@gmail.com>
2026-01-30 12:40:31 +0530

    Add files via upload

---
+++ code7.java
@@ -0,0 +1,14 @@
+class code7{
+    public static void add(float a,float b){
+        float res = a + b;
+        System.out.println(res);
+    }
+    public static void add(int a,int b){
+        int ans = a + b;
+        System.out.println(ans);
+    }
+    public static void main(String[]args){
+        add(30,35);
+        add(30,45);}
+    }
+    
