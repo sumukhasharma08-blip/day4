d5ef3a5d13df3798e755f2bc79bb29b9cf2c2ff4
sumukhasharma08-blip <sumukhasharma08@gmail.com>
2026-01-30 15:00:48 +0530

    Add files via upload

---
+++ code12.java
@@ -0,0 +1,18 @@
+class car{
+    String color;
+    void car()
+    {
+        this.color=color;
+    }
+    void run()
+    {
+        System.out.println("speed");
+    }
+}
+class code12{
+    public static void main(String[]args){
+        car RR = new car();
+        RR.color="white and black";
+        RR.run();
+    }
+}
